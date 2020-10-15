4f145de6b118654555c9c7975e22310268de8f99
Pablo Tovar <pjtf93@gmail.com>
2020-10-15 15:54:59 -0400

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,86 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import React from 'react';
+import { useEffect } from 'react';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
+gsap.registerPlugin(ScrollTrigger);
+import { ScrollToPlugin } from 'gsap/dist/ScrollToPlugin';
+gsap.registerPlugin(ScrollToPlugin);
 
-export default function Home() {
+export default function App() {
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
+      gsap.core.globals('ScrollTrigger', ScrollTrigger);
+      gsap.core.globals('ScrollToPlugin', ScrollToPlugin);
+
+      const firstElem = document.querySelector('.container');
+
+      function goToSection(i, anim) {
+        gsap.to(window, {
+          scrollTo: {
+            y: i * innerHeight + firstElem.offsetTop,
+            autoKill: false,
+          },
+          duration: 1,
+        });
+
+        if (anim) {
+          anim.restart();
+        }
+      }
+      gsap.utils.toArray('.container').forEach((panel, i) => {
+        ScrollTrigger.create({
+          trigger: panel,
+          onEnter: () => goToSection(i),
+        });
+
+        ScrollTrigger.create({
+          trigger: panel,
+          start: 'bottom bottom',
+          onEnterBack: () => goToSection(i),
+        });
+      });
+    }
+
+    return () => {};
+  }, []);
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+    <div className='App'>
+      <div className='full'>
+        <a className='text' href='/Test'>
+          Hello
         </a>
-      </footer>
+      </div>
+
+      <div className='container'>
+        <div className='text'>Hello</div>
+        <div className='box'>
+          <img src='/pexels.jpeg' alt='' />
+        </div>
+      </div>
+      <div className='container'>
+        <div className='text'>Hello</div>
+        <div className='box'>
+          <img src='/pexels.jpeg' alt='' />
+        </div>
+      </div>
+      <div className='container'>
+        <div className='text'>Hello</div>
+        <div className='box'>
+          <img src='/pexels.jpeg' alt='' />
+        </div>
+      </div>
+      <div className='container'>
+        <div className='text'>Hello</div>
+        <div className='box'>
+          <img src='/pexels.jpeg' alt='' />
+        </div>
+      </div>
+
+      <div className='full'>
+        <div className='text'>Hello</div>
+      </div>
     </div>
-  )
+  );
 }
